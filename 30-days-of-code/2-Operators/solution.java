eee4924392e4c26d1ec1f76f2d8e3b4c39831cb6
Christopher Comella <git.chrisco@gmail.com>
2016-10-29 14:40:13 -0400

    Day 2 - Operators

---
+++ 30-days-of-code/2-Operators/solution.java
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/30-operators
+
+public class Arithmetic {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+
+        // Write your calculation code here.
+        double tax = mealCost * taxPercent / 100;
+        double tip = mealCost * tipPercent / 100;
+        double total = mealCost + tax + tip;
+        // cast the result of the rounding operation to an int and save it as totalCost
+        int totalCost = (int) Math.round(total);
+
+        // Print your result
+        System.out.println("The total meal cost is " + totalCost + " dollars.");
+    }
+}
